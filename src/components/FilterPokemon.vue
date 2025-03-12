8d624708f7e3e4cc77f859778355830caefaee80
Nelly <nelly.babillon@gmail.com>
2025-03-12 12:25:04 +0100

    Ajout du composant FilterPokemon pour améliorer la recherche et le filtrage des Pokémon dans HomeView

---
+++ src/components/FilterPokemon.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue'
+
+const emit = defineEmits(['fetch-pokemon'])
+</script>
+
+<template>
+  <h1>Pokedex</h1>
+  <button class="fetch-button p-5 bg-red-500 rounded-md" @click="$emit('fetch-pokemon')">Fetch Pokemon</button>
+
+  <!-- Component Filter -->
+  <div class="filter p-5 text-lg font-semibold text-left rtl:text-right flex gap-5">
+    <input type="text" placeholder="Search for a Pokemon" class="input" />
+    <select id="type"
+      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+      <option selected>Tous</option>
+      <option value="poison">Poison</option>
+      <option value="plante">Plante</option>
+      <option value="eau">Eau</option>
+      <option value="feu">Feu</option>
+    </select>
+  </div>
+</template>
+
+<style scoped>
+  img{
+    max-width: 70px;
+  }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import PokemonList from "../components/PokemonList.vue";
+import FilterPokemon from "../components/FilterPokemon.vue";
 import { ref, computed } from "vue";
 import { fetchPokemon } from "../api/pokemon";
 
@@ -15,22 +16,7 @@ function getPokemons() {
 
 <template>
   <div class="max-w-[80%] shadow-md sm:rounded-lg m-auto text-gray-900 dark:text-white dark:bg-gray-800">
-    <h1>Pokedex</h1>
-    <button class="fetch-button p-5 bg-red-500 rounded-md" @click="getPokemons">Fetch Pokemon</button>
-
-    <!-- Component Filter -->
-    <div class="filter p-5 text-lg font-semibold text-left rtl:text-right flex gap-5">
-      <input type="text" placeholder="Search for a Pokemon" class="input" />
-      <select id="type"
-        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-        <option selected>Tous</option>
-        <option value="poison">Poison</option>
-        <option value="plante">Plante</option>
-        <option value="eau">Eau</option>
-        <option value="feu">Feu</option>
-      </select>
-    </div>
-
+    <FilterPokemon @fetch-pokemon="getPokemons"/>
     <!-- Pokemon Liste -->
     <table class="w-full text-xl text-left text-gray-500 dark:text-gray-400">
       <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
